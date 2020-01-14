7c78e09ebdd5b0bf2b3d767f720f306747dd3fa5
lumakhl <kuhl.luma@gmail.com>
2020-01-13 22:27:10 -0300

    mudança para utilizar githubcontext nos componentes relacionados

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+const Search = ({ setAlert }) => {
+    const githubContext = useContext(GithubContext);
+
     const [text, setText] = useState('');
 
     const onChange = e => {
@@ -13,7 +16,7 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
         if (text === '') {
             setAlert('Please enter something', 'light');
         } else {
-            searchUsers(text);
+            githubContext.searchUsers(text);
             setText('');
         }
     };
@@ -33,17 +36,14 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
                     className='btn btn-dark btn-block'
                 />
             </form>
-            {showClear && (
+            {githubContext.users.length > 0 && (
                 <button className="btn btn-light btn-block"
-                    onClick={clearUsers}>Clear</button>)}
+                    onClick={githubContext.clearUsers}>Clear</button>)}
         </div>
     )
 }
 
 Search.propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
     setAlert: PropTypes.func.isRequired
 };
 
